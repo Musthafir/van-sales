95379c187368d233f815ae896f88b7948289b5d0
MUSTHAFFIR <musthaffirp@gmail.com>
2026-09-23 22:06:12 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'mahrjan-v4-5';
+const CACHE = 'mahrjan-v4-6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c => Promise.all(ASSETS.map(a => c.add(a).catch(() => null)))).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
